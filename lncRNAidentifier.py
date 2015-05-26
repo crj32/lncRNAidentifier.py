e3f873dc157ec47990bcbc7a90aa5891089576b5
crj32 <chris.r.john86@gmail.com>
2015-05-26 08:07:40 +0100

    Update lncRNAidentifier.py

--- lncRNAidentifier.py
+++ lncRNAidentifier.py
@@ -1,25 +1,27 @@
-# noncodingRNAfinder2015
+# lncRNAidentifier.py (written by Christopher R John and Ivan Reyna-Llorens)
 
 # Prerequisites
 
 # (for the usearch blast) Download usearch (http://www.drive5.com/usearch/download.html), follow installation instructions, and put executable in bin directory 
-# (for the usearch blast) The current working directory has to have 3 proteome databases called db.udb (created with Usearch by using 'usearch -makeudb_ublast transcripts.fasta -output db.udb')
-# and db2.udb (tRNA+rRNA database)
+# (for the usearch blast) The current working directory has to have 2 usearch blast databases in called db.udb (for the swiss protein database) and db2.udb (for the mixed RNA database)
+# You can create this with the following command 'usearch -makeudb_ublast transcripts.fasta -output db.udb'
 # (for PLEK) Download from sourceforge straight into bin directory (http://sourceforge.net/projects/plek/files/) and follow installation instructions for install inside bin
 
 # Steps
 
-# Note that Usearch and PLEK will not run if their output files are found already (they run on the full transcriptomes)
-# Run script.py --help for the flags # this needs a proper options menu
-# Set up in script parameters before running the program
+# Set the -i and -c flags please
+# The only in script parameter is now the e value - do not change this unless testing etc - it should be fine
+# Note the fasta header is important and should be in the format 'TRANSxxxxx gene=xxxxxx' (note the space inbetween the two strings (change with sed e.g.  sed 's/|/ /' xaa))
+# If there are problems with the fasta header please use sed to modify this prior to running the program
 
 # Steps 1&2: filter out anything under < 200bp & max ORF size < 100aa
 # Step 3: run a blast search against a protein database (http://www.uniprot.org/) and remove transcripts that hit with e < 0.001
-# Step 4: run PLEK to get the ncRNAs (may still include miRNA precursors, snRNA, snoRNA, tRNA, rRNA)
-# Step 5: run a usearch against a tRNA database (arabidopsis and rice tRNA database - ensembl) - merged
-# Step 6: run a usearch against a rRNA database (arabidopsis and rice rRNA database - ensembl) - merged
-# Step 7: run a usearch against a microRNA database (arabidopsis and rice microRNAs database - ensembl) - standalone (different e value required? needs testing)
+# Step 4: run PLEK to get the lncRNAs (may still include contaminating miRNA precursors, snRNA, snoRNA, tRNA, rRNA)
+# Step 5: run a usearch against a tRNA database (arabidopsis and rice tRNA database - ensembl) - merged in db2.ub
+# Step 6: run a usearch against a rRNA database (arabidopsis and rice rRNA database - ensembl) - merged in db2.ub
+# Step 7: run a usearch against a microRNA database (http://bioinformatics.cau.edu.cn/PMRD/ all species - mature sequences) - merged in db2.ub
 
+from __future__ import division
 from Bio.Seq import Seq
 from Bio import SeqIO
 import re
@@ -27,18 +29,11 @@ import subprocess as sp
 import os
 from os import path
 import optparse
-from pylab import *
 
-# parameters 
+# parameters
 
-transcriptomefilename = 'bignucsnamed.fasta'
-inputfilename = 'bignucsnamed.fasta'
-cores = '2'
-evalue = '1e-3' # for blast against swiss prot database
-
-# open file
-
-inputfile = open(inputfilename, "rU")
+evalue = '1e-3' # for blast against swiss prot database & master RNA database
+blast_uniprot = 'ON' # optional blast search against swiss protein database (db.udb) 'ON' or 'OFF' - may increase true positives, but increase false negatives
 
 # globals
 
@@ -54,7 +49,7 @@ def translate(): # translate in all 3 frames ignoring stop codons
 	original_count = 0
 	translate.passed_transcripts = 0
 	print 'Filtering for transcripts > 200bp...'
-	for record in SeqIO.parse(inputfile, "fasta"):
+	for record in SeqIO.parse(options.inputfilename, "fasta"):
 		original_count += 1
 		if len(record) >= 200:
 			translate.passed_transcripts += 1
@@ -63,7 +58,8 @@ def translate(): # translate in all 3 frames ignoring stop codons
 	print 'Original transcripts: ', original_count
 	print 'Longer than 200bp: ', translate.passed_transcripts
 	global result_summary
-	result_summary += '\n\nResults: First round of filtering\n'+'Original transcripts: '+str(original_count)
+	result_summary += 'Processing '+options.inputfilename+' with lncRNAidentifier.py'+'\n\nResults: First round of filtering\n'+'Original transcripts: '+str(original_count)+'\nFiltered transcripts: '+str(translate.passed_transcripts)
+	translate.original_count = original_count
 
 def calculateORFs(seq): # to output ORF maximum length for each reading frame
 	string = str(seq)
@@ -121,45 +117,51 @@ def findORFs(): # find the biggest ORFs
 
 	print 'These are the number of ids to keep after first 2 steps:', len(findORFs.ids_tokeep), '\n'
 
-def blast(): # do the blast search on transcriptsandgenes.fa
-	if not os.path.exists('hits.m8'):
+def blast(): # do the blast search on swiss protein database
+	if blast_uniprot == 'ON':
+		#if not os.path.exists('hits.m8'):
 		usearch = 'usearch'
 		print '\nRunning Usearch on filtered transcripts...\n'
-		proc = sp.Popen([usearch, '-ublast', transcriptomefilename, '-db', 'db.udb', '-evalue', evalue, '-accel', '0.5', '-userout', 'hits.m8', '-strand', 'both', '-userfields', 'query+target+id+evalue+bits'])
+		proc = sp.Popen([usearch, '-ublast', options.inputfilename, '-db', 'db.udb', '-evalue', evalue, '-accel', '0.5', '-userout', 'hits.m8', '-strand', 'both', '-userfields', 'query+target+id+evalue+bits'])
 		proc.wait()
 		print 'Done searching\n'
 
-	# read in the blast results & get the IDs to get rid of
+		# read in the blast results & get the IDs to get rid of
 
-	hits = open('hits.m8', 'r')
-	ids_todelete = []
+		hits = open('hits.m8', 'r')
+		ids_todelete = []
 
-	for line in hits:
-		v1, v2, v3, v4, v5 = line.split('\t')
-		ids_todelete.append(v1)
+		for line in hits:
+			v1, v2, v3, v4, v5 = line.split('\t')
+			ids_todelete.append(v1)
 
-	ids_todelete2 = list(set(ids_todelete))
+		ids_todelete2 = list(set(ids_todelete))
 
-	# filter those ids out of transcripts to keep
+		blast.ids = list(set(ids_todelete)) ################################# TEMPORARY CODE
 
-	blast.ids_tokeep2 = []
-	for idx in findORFs.ids_tokeep:
-		if idx not in ids_todelete2:
-			blast.ids_tokeep2.append(idx)
-	numberdeleted = (len(findORFs.ids_tokeep)) - (len(blast.ids_tokeep2))
+		# filter those ids out of transcripts to keep
 
-	print 'Results: third round of filtering\n'
-	print 'Deleted', numberdeleted, 'transcripts that match a protein with a evalue of 1e-3'
-	print 'These are the number of ids to keep after first 3 steps:', len(blast.ids_tokeep2)
-	global result_summary
-	result_summary += '\n\nResults: third round of filtering\n'+'Deleted '+str(numberdeleted)+' transcripts that match a protein with a evalue of 1e-3'+'\nThese are the number of ids to keep after first 3 steps: '+str(len(blast.ids_tokeep2))
+		blast.ids_tokeep2 = [] # this is what leaves for nxt func
+		for idx in findORFs.ids_tokeep:
+			if idx not in ids_todelete2:
+				blast.ids_tokeep2.append(idx)
+		numberdeleted = (len(findORFs.ids_tokeep)) - (len(blast.ids_tokeep2))
 
-def PLEK(): # run PLEK on the transcriptome # need to sort out -model and -range for your model
+		print 'Results: third round of filtering\n'
+		print 'Deleted', numberdeleted, 'transcripts that match a protein with a evalue of 1e-3'
+		print 'These are the number of ids to keep after first 3 steps:', len(blast.ids_tokeep2)
+		global result_summary
+		result_summary += '\n\nResults: third round of filtering\n'+'Deleted '+str(numberdeleted)+' transcripts that match a protein with a evalue of 1e-3'+'\nThese are the number of ids to keep after first 3 steps: '+str(len(blast.ids_tokeep2))
+	else:
+		blast.ids_tokeep2 = []
+		blast.ids_tokeep2 = findORFs.ids_tokeep		
+
+def PLEK(): # run PLEK on the transcriptome # -model and -range currently are default
 	print '\nRunning PLEK on transcripts...\n'
-	if not os.path.exists('./predicted'):
-		PLEK = 'PLEK.py'
-		proc = sp.Popen([PLEK, '-fasta', inputfilename, '-out', 'predicted', '-thread', cores])
-		proc.wait()
+	#if not os.path.exists('./predicted'):
+	PLEK = 'PLEK.py'
+	proc = sp.Popen([PLEK, '-fasta', options.inputfilename, '-out', 'predicted', '-thread', options.cores])
+	proc.wait()
 	print '\nDone\n'
 
 	# process PLEK data
@@ -170,8 +172,8 @@ def PLEK(): # run PLEK on the transcriptome # need to sort out -model and -range
 	blast.PLEK_tokeep = []
 
 	for line in PLEK_results:
-		v1, v2, v3 = line.split('\t')
-		v3 = v3[1:15]
+		v1, v2, v3, v4 = re.split(r'[\t ]+', line) # this depends on the fasta header formating
+		v3 = v3[1:]						
 		PLEK_dict[v3] = [v1, v2]
 
 	PLEK_non_coding_transcripts = {}
@@ -179,8 +181,6 @@ def PLEK(): # run PLEK on the transcriptome # need to sort out -model and -range
 
 	PLEK_ncRNAs = 0
 	PLEK_count = 0
-
-	print blast.ids_tokeep2 # where are these going?
 
 	for k, v in PLEK_dict.iteritems():
 		if v[0] == 'Non-coding':
@@ -199,21 +199,20 @@ def PLEK(): # run PLEK on the transcriptome # need to sort out -model and -range
 			final[k] = [v[0], v[1]]
 
 	print 'Results: fourth round of filtering\n'
-	#print 'PLEK found', PLEK_ncRNAs, 'transcripts that identified as ncRNAs in the original transcriptome'
 	print 'Deleted', finalnumberdeleted, 'transcripts that PLEK did not judge as being long non coding RNAs'
-	print '\nThese are the number of transcripts left after PLEK,', PLEK_count
+	print 'These are the number of transcripts left after PLEK,', PLEK_count
 	global result_summary
 	result_summary += '\n\nResults: fourth round of filtering\n'+'Deleted '+str(finalnumberdeleted)+' transcripts that PLEK did not judge as being long non coding RNAs'+'\nThese are the number of transcripts left after PLEK, '+str(PLEK_count)
 
 # remove tRNA, rRNA, and microRNA precursors
 
-def blast2(): # do the blast search on transcriptsandgenes.fa
-	if not os.path.exists('hits2.m8'):
-		usearch = 'usearch'
-		print '\nRunning Usearch on filtered transcripts...\n'
-		proc = sp.Popen([usearch, '-ublast', transcriptomefilename, '-db', 'db2.udb', '-evalue', evalue, '-accel', '0.5', '-userout', 'hits2.m8', '-strand', 'both', '-userfields', 'query+target+id+evalue+bits'])
-		proc.wait()
-		print 'Done searching\n'
+def blast2():
+	#if not os.path.exists('hits2.m8'):
+	usearch = 'usearch'
+	print '\nRunning Usearch on filtered transcripts...\n'
+	proc = sp.Popen([usearch, '-ublast', options.inputfilename, '-db', 'db2.udb', '-evalue', evalue, '-accel', '0.5', '-userout', 'hits2.m8', '-strand', 'both', '-userfields', 'query+target+id+evalue+bits'])
+	proc.wait()
+	print 'Done searching\n'
 
 	# read in the blast results & get the IDs to get rid of
 
@@ -249,25 +248,34 @@ def blast2(): # do the blast search on transcriptsandgenes.fa
 			
 	#
 
+	final_count = len(blast_ids_tokeep_x)
+	percentremaining = (final_count/translate.original_count)*100
+
 	print '\nResults: fifth round of filtering\n'
 	print 'Deleted', numberdeleted, 'transcripts that match a rRNA, miRNA, tRNA, snRNA, or snoRNA with a evalue of 1e-3'
 	print 'These are the number of ids to keep after the 5 steps:', len(blast_ids_tokeep_x)
 	global result_summary
 	result_summary += '\n\nResults: fifth round of filtering\n'+'Deleted '+str(numberdeleted)+' transcripts that match a rRNA, tRNA, or microRNA with a evalue of 1e-3'+'\nThese are the number of ids to keep after first 5 steps: '+str(len(blast_ids_tokeep_x))
+	print '\nFinal results\nOriginal transcripts: ', translate.original_count, '\nFinal transcripts: ', len(blast_ids_tokeep_x), '\nPercent remaining: ', percentremaining
+	result_summary += '\n\nFinal results\nOriginal transcripts: '+str(translate.original_count)+'\nFinal transcripts: '+str(len(blast_ids_tokeep_x))+'\nPercent remaining: '+str(percentremaining)
 
 def print_results(): # printing output files
 	# id list with scores from PLEK
+	outputname2 = options.inputfilename+'_results.csv'
+	outputname = re.sub('.fa', "", outputname2)
 	print '\nwriting non coding RNA results file...'
-	with open('noncodingRNAresults.csv', 'w') as out_file:
+	with open(outputname, 'w') as out_file:
 		for k, v in final.iteritems():
 			towrite = k+'\t'+v[0]+'\t'+v[1]+'\n'
 			out_file.write(towrite)
 
 def print_fasta(): # printing an output fasta
+	outputname2 = options.inputfilename
+	outputname = re.sub('.fa', "", outputname2)+'_sequences.fasta'
 	print 'writing non coding RNA transcripts to fasta... (may take some time)'
 	newdict = {}
-	for record in SeqIO.parse(transcriptomefilename, "fasta"):
-		if record.id in final:
+	for record in SeqIO.parse(options.inputfilename, "fasta"):
+		if record.id in final: 
 			newdict[record.id] = record.seq
 	towrite = ''
 	for k, v in newdict.iteritems():
@@ -275,20 +283,24 @@ def print_fasta(): # printing an output fasta
 		towrite += '>'+k+'\n'
 		for i in range(0,length,80):
 			towrite += v[i:i+80]+'\n'
-	with open ("putative_ncRNAs.fa", 'w') as output_file:
+	with open (outputname, 'w') as output_file:
 		output_file.write(str(towrite))	
 
 def print_resultsummary():
+	outputname2 = options.inputfilename+'_summary.txt'
+	outputname = re.sub('.fa', "", outputname2)
 	print 'writing ncRNA results summary'
-	with open('result_summary.txt', 'w') as out_file:
+	with open(outputname, 'w') as out_file:
 		out_file.write(result_summary)
 		
-def main(): # get options and run script # this needs to be done properly
+def main():
 	parser = optparse.OptionParser()
+
+	parser.add_option('-i', '--input', dest='inputfilename', metavar='inputfilename', help='fasta input file name')
+	parser.add_option('-c', '--cores', dest='cores', metavar='cores', help='how many cores to use')
 	parser.add_option('-f', '--fasta', action="store_true", dest="verbose", help="output putative ncRNAs to a fasta file")
-	parser.add_option('-s', '--summary', action="store_true", dest="verbose2", help="output a results summary")
-	parser.add_option('-g', '--graphics', action="store_true", dest="verbose3", help="output graphical results files")
-	
+
+	global options
 	(options, args) = parser.parse_args()
 
 	def core():
@@ -298,32 +310,13 @@ def main(): # get options and run script # this needs to be done properly
 		PLEK()
 		blast2()	
 		print_results()
+		print_resultsummary()
 
-	if options.verbose and not options.verbose2 and not options.verbose3:
+	if options.verbose:
 		core()
-		print_fasta()
-	elif options.verbose2 and not options.verbose and not options.verbose3:
-		core()
-		print_resultsummary()
-	elif options.verbose3 and not options.verbose2 and not options.verbose:
-		core()
-		# make_figures()
-	elif options.verbose2 and options.verbose and not options.verbose3:
-		core()
-		print_fasta()
-		print_resultsummary()
-	elif options.verbose2 and options.verbose3 and not options.verbose:
-		core()
-		print_resultsummary()
-		#make_figures()
-	elif options.verbose3 and options.verbose and not options.verbose2:
-		core()
-		#make_figures()
 		print_fasta()
 	else:
 		core()
-		print_resultsummary()
-		#make_figures()
 
 if __name__ == "__main__":
 	main()		   	
